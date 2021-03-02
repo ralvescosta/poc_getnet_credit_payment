d5c8c03b5e889112b4c87fb8b07f9ee9319115be
Rafael Costa <rafael.rac.mg@gmail.com>
2021-03-01 21:19:30 -0300

    refactor: http client handler and payment dto

--- Configurations/Configurations.cs
+++ Configurations/Configurations.cs
@@ -4,7 +4,7 @@
     {
         public string GetNetAuthEnpoint = "https://api-sandbox.getnet.com.br/auth/oauth/v2/token";
         public string GetNetCardTokenizationEntpoint = "https://api-sandbox.getnet.com.br/v1/tokens/card";
-        public string GetNetClientId = "9bf68dc9-5dd0-4d2e-89d9-fdcce2796899";
-        public string GetNetClientSecrete = "5bb4dad3-0205-4529-9d17-7aaae38e7311";
+        public string GetNetClientId = "";
+        public string GetNetClientSecrete = "";
     }
 }
